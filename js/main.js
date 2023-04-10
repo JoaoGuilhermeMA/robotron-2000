55b06d88f7fdf218fccca53e510f781b6a639be5
JoaoGuilhermeMA <joomoreira36@gmail.com>
2023-04-10 13:42:17 -0300

    criando e alterando braços

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
-const robotron = document.querySelector("#robotron");
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
 
-robotron.addEventListener("click", (evento) => {
-    console.log(evento)
-})
+somar.addEventListener("click", () => {manipulaDados("soma")})
 
-function dizOi(nome) {
-    console.log("Oi " + nome)
-    console.log("Bem-vindo ao Robotron 2000")
+subtrair.addEventListener("click", () => {manipulaDados("subtrair")})
+
+function manipulaDados(operacao){
+    if(operacao == "subtrair"){
+        braco.value = parseInt(braco.value) - 1
+    }else{
+        braco.value = parseInt(braco.value) + 1
+    }
 }
-
-dizOi("João");
-
