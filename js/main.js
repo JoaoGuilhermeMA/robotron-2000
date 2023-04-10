24da61119237f7b9efaa199669c4724a6c479dbd
JoaoGuilhermeMA <joomoreira36@gmail.com>
2023-04-10 12:50:08 -0300

    criando a primeira interação ja com html

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 const robotron = document.querySelector("#robotron");
 
-robotron.addEventListener("click", dizOi)
+robotron.addEventListener("click", (evento) => {
+    console.log(evento)
+})
 
-function dizOi() {
-    console.log("Oi")
+function dizOi(nome) {
+    console.log("Oi " + nome)
+    console.log("Bem-vindo ao Robotron 2000")
 }
 
+dizOi("João");
 
